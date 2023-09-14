ffa259cd51c23d753d9d839b9072814cee5e1e1a
Juan17Informatico <juancampuzano2356@gmail.com>
2023-09-14 09:28:06 -0500

    Se agrego un nuevo diseño responsive para dispositivos mobiles. Nuevo color background y se aggregan variables CSS.

--- analisis_salarial/reto.js
+++ analisis_salarial/reto.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const inputIngresos = document.getElementById('inputIngresos');
 const inputGastos = document.getElementById('inputGastos');
 const inputAporteDeuda = document.getElementById('inputAporteDeuda');
@@ -14,21 +13,4 @@ function calcularCapacidadEndeudamiento(){
     pResult.innerText = capacidadEndeudamiento;
 }
 
-=======
-const inputIngresos = document.getElementById('inputIngresos');
-const inputGastos = document.getElementById('inputGastos');
-const inputAporteDeuda = document.getElementById('inputAporteDeuda');
-const btnCalcular = document.getElementById('btnCalcular');
-const pResult = document.getElementById('pResult');
-
-function calcularCapacidadEndeudamiento(){
-    const ingresos = Number(inputIngresos.value);
-    const gastos = Number(inputGastos.value);
-    const aporteDeuda = Number(inputAporteDeuda.value);
-    const capacidadEndeudamiento = (ingresos - gastos - aporteDeuda) * 0.35;
-    // console.log(capacidadEndeudamiento);
-    pResult.innerText = capacidadEndeudamiento;
-}
-
->>>>>>> 0aa921b963b1160506c8ee5b6097c82b53300e5b
 btnCalcular.addEventListener('click', calcularCapacidadEndeudamiento);
