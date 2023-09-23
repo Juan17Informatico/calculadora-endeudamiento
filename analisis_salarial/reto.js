33a4a17600c55865b8e734552a345bf2a38e6b6f
Juan17Informatico <juancampuzano2356@gmail.com>
2023-09-22 22:37:33 -0500

    Realizada conversion de numero simple a formato numerico contable utilizando el objeto Intl y su constructor NumberFormat. Se implemento una funcion que acepta dos parametros para proporcionar un resultado que incluye el tipo de moneda y el numero convertido.

--- analisis_salarial/reto.js
+++ analisis_salarial/reto.js
@@ -1,16 +1,36 @@
-const inputIngresos = document.getElementById('inputIngresos');
-const inputGastos = document.getElementById('inputGastos');
-const inputAporteDeuda = document.getElementById('inputAporteDeuda');
-const btnCalcular = document.getElementById('btnCalcular');
-const pResult = document.getElementById('pResult');
+// Inicialización y declaración de variables
+const inputIngresos = document.getElementById('inputIngresos'); // Elemento de entrada de ingresos
+const inputGastos = document.getElementById('inputGastos'); // Elemento de entrada de gastos
+const inputAporteDeuda = document.getElementById('inputAporteDeuda'); // Elemento de entrada de aporte deuda
+const btnCalcular = document.getElementById('btnCalcular'); // Botón para calcular
+const pResult = document.getElementById('pResult'); // Párrafo para mostrar el resultado
 
-function calcularCapacidadEndeudamiento(){
+/**
+ * Convierte un número en formato de moneda.
+ * @param {number} number - El número que se desea formatear.
+ * @param {string} currency - La moneda en la que se desea formatear (por ejemplo, 'USD' para dólares).
+ * @returns {string} - El número formateado en formato de moneda.
+ */
+function numberConverter(number, currency) {
+    const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: `${currency}` });
+    return formatter.format(number);
+}
+
+/**
+ * Calcula la capacidad de endeudamiento basándose en ingresos, gastos y aportes a deuda.
+ */
+function calcularCapacidadEndeudamiento() {
+    // Obtener valores de las entradas y convertirlos a números
     const ingresos = Number(inputIngresos.value);
     const gastos = Number(inputGastos.value);
     const aporteDeuda = Number(inputAporteDeuda.value);
+    
+    // Calcular la capacidad de endeudamiento según la fórmula específica
     const capacidadEndeudamiento = (ingresos - gastos - aporteDeuda) * 0.35;
-    // console.log(capacidadEndeudamiento);
-    pResult.innerText = capacidadEndeudamiento;
+    
+    // Mostrar el resultado en el párrafo pResult en formato de moneda
+    pResult.innerText = numberConverter(capacidadEndeudamiento, 'USD');
 }
 
+// Agregar un evento click al botón "Calcular" que llama a la función calcularCapacidadEndeudamiento
 btnCalcular.addEventListener('click', calcularCapacidadEndeudamiento);
