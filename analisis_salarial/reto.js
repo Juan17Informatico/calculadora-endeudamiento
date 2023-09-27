2231d59a6f47d686b478f06d25b71783fa3312ea
Juan17Informatico <juancampuzano2356@gmail.com>
2023-09-26 22:35:00 -0500

    Se incorporo una funcion y una mejor estructuracion para el codigo. la funcion sirve especificamente para validar el tipo de valor de los numeros y verificar que si sean numeros validos y no valores tipo string.

--- analisis_salarial/reto.js
+++ analisis_salarial/reto.js
@@ -17,20 +17,42 @@ function numberConverter(number, currency) {
 }
 
 /**
+ * Valida que el valor ingresado sea un número, de lo contrario retorna 0.
+ * @param {string} input - El valor ingresado.
+ * @returns {number} - El valor ingresado convertido a número o 0 si no es un número.
+ * */
+function validationInputNumberResult(input) {
+    if (typeof input === 'number' && !isNaN(input)) {
+        return input;
+    } else {
+        return 0;
+    }
+}
+
+/**
  * Calcula la capacidad de endeudamiento basándose en ingresos, gastos y aportes a deuda.
  */
 function calcularCapacidadEndeudamiento() {
-    // Obtener valores de las entradas y convertirlos a números
-    const ingresos = Number(inputIngresos.value);
-    const gastos = Number(inputGastos.value);
-    const aporteDeuda = Number(inputAporteDeuda.value);
+    //validar input numeros, esto nos ayudara a que no se pueda ingresar letras o caracteres especiales, la validacion se realiza con la funcion validationInputNumberResult
+    const ingresos = validationInputNumberResult(Number(inputIngresos.value));
+    const gastos = validationInputNumberResult(Number(inputGastos.value));
+    const aporteDeuda = validationInputNumberResult(Number(inputAporteDeuda.value));
+
+    console.log({ingresos,gastos, aporteDeuda });//para ver los valores que se estan ingresando en los input
+
+    if (ingresos === 0 || gastos === 0 ||  aporteDeuda === 0) {
+        pResult.innerText = 'Ingrese valores válidos';
+        return;
+    }
     
     // Calcular la capacidad de endeudamiento según la fórmula específica
     const capacidadEndeudamiento = (ingresos - gastos - aporteDeuda) * 0.35;
-    
+
     // Mostrar el resultado en el párrafo pResult en formato de moneda
     pResult.innerText = numberConverter(capacidadEndeudamiento, 'USD');
 }
+
+
 
 // Agregar un evento click al botón "Calcular" que llama a la función calcularCapacidadEndeudamiento
 btnCalcular.addEventListener('click', calcularCapacidadEndeudamiento);
