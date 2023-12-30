d2d80a53fc6887b824914ac5d6ef592edc0d88ae
Juan17Informatico <juancampuzano2356@gmail.com>
2023-12-30 16:16:26 -0500

    Se inicio y finalizo la logica de porcentajes

--- porcentajes/porcentajes.js
+++ porcentajes/porcentajes.js
@@ -1,18 +1,20 @@
 const inputPrice = document.querySelector('#price');
-const inputCoupon = document.querySelector('#coupon');
-const btn = document.querySelector('#calcular');
+const inputCoupon25 = document.querySelector('#buttonCoupon1');
+const inputCoupon30 = document.querySelector('#buttonCoupon2');
+const inputCoupon50 = document.querySelector('#buttonCoupon3');
+const pResultCupon = document.querySelector('#resultCupon')
 const pResult = document.querySelector('#result')
 //Cupones
 let couponList = [];
 couponList.push({
   name: "Cupon 1",
-  discount: 30,
-  
+  discount: 25,
+
 });
 
 couponList.push({
   name: 'Cupon 2',
-  discount: 20,
+  discount: 30,
 });
 
 couponList.push({
@@ -20,53 +22,49 @@ couponList.push({
   discount: 50,
 });
 
-btn.addEventListener('click', calcularPrecioConDescuento)
-
-function calcularPrecioConDescuento() {
+// btn.addEventListener('click', calcularPrecioConDescuento)
+inputCoupon25.addEventListener('click', () => {
   const price = Number(inputPrice.value)
-  const coupon = inputCoupon.value
-  let discount
+  const coupon = couponList[0].discount;
+  
+  const resultPrice = calcularPrecioDescuento(coupon, price);
+  
+  pResultCupon.innerText = coupon;
+  pResult.innerText = resultPrice
+  
+})
 
-  if (!price || !coupon) {
+inputCoupon30.addEventListener('click', () => {
+  const price = Number(inputPrice.value)
+  const coupon = couponList[1].discount;
+  
+  const resultPrice = calcularPrecioDescuento(coupon, price);
+  
+  pResultCupon.innerText = coupon;
+  pResult.innerText = resultPrice
+  
+})
+
+inputCoupon50.addEventListener('click', () => {
+  const price = Number(inputPrice.value)
+  const coupon = couponList[2].discount;
+  
+  const resultPrice = calcularPrecioDescuento(coupon, price);
+  
+  pResultCupon.innerText = coupon;
+  pResult.innerText = resultPrice
+  
+})
+
+function calcularPrecioDescuento(discount, price) {
+
+  if (!price || !discount) {
     pResult.innerText = 'Por favor llena el formulario'
     return
   }
 
-  function encontrarCupon(couponElement){ // {name, discount}
-    return couponElement.name == coupon; //Preguntamos si la propiedad .name del array es igual al valor del "coupon"
-  }
 
-  // const couponInArray = couponList.filter(encontrarCupon); El metodo filter nos devuelve un array con el objeto que cumple la validacion
-  const couponInArray = couponList.find(encontrarCupon); // El metodo find nos devuelve el objeto con los valores que cumplen la validacion
+  return (price * (100 - discount)) / 100
 
-  if(couponInArray){
-    discount = couponInArray.discount;
-  }else{
-    pResult.innerText = 'El cupón no es válido';
-    return;
-  }
-
-  console.log(
-    discount,
-    couponInArray,
-  ); 
-
-  // for (let i = 0; i < arrayCuponesN.length; i++) {
-  //   for (const key in arrayCuponesN[i]) {
-  //     console.log('1', arrayCuponesN[i].name)
-  //     let cupones = arrayCuponesN[i].name
-  //     if (cupones == coupon) {
-  //       discount = arrayCuponesN[i].discount
-  //     }
-
-  //   }
-  // }
-
-  if (discount > 100) {
-    pResult.innerText = 'El descuento no puede ser mayor a 100%'
-    return
-  }
-
-  const newPrice = (price * (100 - discount)) / 100
-  pResult.innerText = 'El nuevo precio con descuento es $' + newPrice
 }
+
